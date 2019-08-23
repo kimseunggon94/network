ec96424891a06e75a971667634b652e4f9f06932
venture21 <park0122@gmail.com>
2019-08-23 11:34:38 +0900

    EchoServer Mutithread 구현

--- src/echo/EchoServer.java
+++ src/echo/EchoServer.java
@@ -1,15 +1,10 @@
 package echo;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class EchoServer {
 	private static final int PORT = 8000;
@@ -28,39 +23,11 @@ public class EchoServer {
 			log("binding " + inetAddress.getHostAddress() + ":" + PORT);
 			
 			//3. accept
-			Socket socket = serverSocket.accept();
-			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
-			log("connected from client[" + inetRemoteSocketAddress.getAddress().getHostAddress() + ":" + inetRemoteSocketAddress.getPort() + "]");
-			
-			try {
-				//4. I/O Stream생성
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
-
-				while(true) {
-					
-					//5. 데이터 읽기(수신)
-					String data = br.readLine();
-					if(data == null) {
-						log("closed by client");
-						break;
-					}
-					log("received:" + data);
-				
-					//6. 데이터 쓰기(송신)
-					pw.println(data);
-				}
-				
-			} catch(SocketException e) {
-				log("abnormal closed by client");
-			} catch(IOException e) {
-				e.printStackTrace();
-			} finally {
-				//7. Socket 자원정리
-				if(socket != null && socket.isClosed() == false) {
-					socket.close();
-				}
+			while(true) {
+				Socket socket = serverSocket.accept();
+				new EchoServerReceiveThread(socket).start();
 			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -76,6 +43,6 @@ public class EchoServer {
 	}
 
 	public static void log(String log) {
-		System.out.println("[Echo Server] " + log);
+		System.out.println("[Echo Server#" + Thread.currentThread().getId() + "] " + log);
 	}
 }
